cdd3fce90f362c33a1eb04a7cde28ef46b7517b1
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-05-01 12:46:41 +0100

    don't trust code from chatgpt

--- components/Lander.tsx
+++ components/Lander.tsx
@@ -2,10 +2,12 @@
 export default function Lander(){
 return (
     <>
-      <div className=" bg-red-500 bg-opacity-50 w-full relative">
-        <img src={'/cross.jpg'} className="w-full h-screen opacity-50"  alt="image"/>
-        <div className="inset-1/2 absolute ">
-          <p >Hello how are you</p>
+      <div className=" bg-red-500 bg-opacity-50 min-h-screen flex items-center w-full relative">
+        <img src={'/cross.jpg'} className="w-full h-screen opacity-50"  alt="this image shows on my machine"/>
+        <div className="top-1/2 left-1/2 absolute ">
+          <div className="flex flex-col bg-white">
+            <p >Hello how are you more fillers, read the bible your way heyy</p>
+          </div>
         </div>
       </div>
     </>
