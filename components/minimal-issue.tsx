2b0b68b44824a7aef75fc78d26a5bf6ec52fcee4
Shub <78972053+shubhankartrivedi@users.noreply.github.com>
2024-05-04 03:32:05 +0530

    Update minimal-issue.tsx

--- components/minimal-issue.tsx
+++ components/minimal-issue.tsx
@@ -1,14 +1,12 @@
 import Image from "next/image";
-
+import imgSrc from "../public/cross.jpg";
 export default function LanderReproduce(){
     return (
-        <>
           <div className=" bg-red-500 bg-opacity-50 w-full relative">
-            <Image src={'/cross.jpg'} className="w-full h-screen opacity-50" height={876} width ={544} alt="image"/>
+            <Image src={imgSrc} className="w-full h-screen opacity-50" height={876} width={544} alt="image"/>
             <div className="inset-1/2 absolute ">
-              <p >Hello how are you</p>
+              <p>Hello how are you</p>
             </div>
           </div>
-        </>
     )
     }
