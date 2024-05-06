65638e82dc2b423ec3b9ea3f797e60b8677f5095
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-05-06 14:17:16 +0100

    swiper js

--- components/section.tsx
+++ components/section.tsx
@@ -9,7 +9,7 @@ return (
     <>
       <div className="  section bg-cover bg-center bg-opacity-50 min-h-96 flex items-center w-full">
         {/* <Image src={cross} className="w-full  h-screen opacity-50 sticky" width={344} height={344} alt="this image shows on my machine"/> */}
-        <div className="h-96 bg-slate-500 bg-opacity-50 w-full">
+        <div className="h-96 bg-slate-500 flex justify-center items-center bg-opacity-50 w-full">
           <div className="flex flex-col ">
             <MotionP
             className={`${rob} text-white`}
