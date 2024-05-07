04ddbb5bc0b43b40039be90a457a80a2c1dc5551
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-05-07 12:11:48 +0100

    swiper actually has a bug

--- components/Lander.tsx
+++ components/Lander.tsx
@@ -1,18 +1,17 @@
 import Image from "next/image";
 import { MotionP } from "./motions";
 import { rob } from "./Nav";
-import cross from '@/public/cross.jpg'
+import SwiperComponent from "./swipes";
+
+
 export default function Lander(){
 return (
     <>
       <div className="  bg-cover bg-center   min-h-screen flex items-center w-full lander">
+      
         {/* <Image src={cross} className="w-full h-screen -z-10 opacity-50 sticky  " width={344} height={344} alt="this image shows on my machine"/> */}
-        <div className="bg-red-500 bg-opacity-50 flex justify-center items-center p-4 w-full h-screen">
-          <div className="flex flex-col ">
-            <MotionP
-            className={`${rob} text-white`}
-            >Hello how are you more fillers, read the bible your way heyy</MotionP>
-          </div>
+        <div className="bg-red-500 bg-opacity-50 flex  justify-center items-center p-4 w-full h-screen">
+            <SwiperComponent />
         </div>
       </div>
     </>
--- components/swipes.tsx
+++ components/swipes.tsx
@@ -1,1 +1,35 @@
+"use client";
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+export default function SwiperComponent(){
+    let strings: string[] = ['hello filler', 'or fillers', 'na some filers lol'];
+     return(
+        <>
+           <Swiper
+            modules={[Navigation, Pagination, Autoplay]}
+            pagination={{ clickable: true }}
+            loop={true}
+            spaceBetween={0}
+            slidesPerView={1}
+            autoplay={{
+                delay:1500,
+             }}
+           >
+                {strings.map((str, i)=>{
+                    return (
+                        <SwiperSlide className='w-full p-10' key={i}>
+                            <div className='flex items-center p-24 justify-center'>
+                            <p className='text-2xl text-white'>{str}</p> 
+                            </div>
+                        </SwiperSlide>
+                    )
+                })}
+             
+           </Swiper>
+        </>
+     )
+}
