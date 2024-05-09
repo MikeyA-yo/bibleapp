19df1963fa53cb475455a836da766f6dc50367a4
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-05-09 19:23:15 +0100

    added some things

--- components/features.tsx
+++ components/features.tsx
@@ -6,7 +6,7 @@ export default function Features() {
       <div className=" feature h-auto  bg-cover bg-center ">
         <div className="flex items-center py-6  lg:justify-center md:justify-center flex-wrap justify-evenly w-full lg:gap-20 md:gap-15 gap-8 lg:flex-row md:flex-row flex-col h-full bg-neutral-900 bg-opacity-60">
           <MotionDiv
-            className="bg-slate-600  rounded-3xl p-10 lg:p-8 md:p-8 bg-opacity-65"
+            className="bg-slate-600  rounded-3xl p-10 m-3 lg:p-8 md:p-8 bg-opacity-65"
             initial={{ y: "100%", opacity: 0.3, backdropFilter: "blur(10px)" }}
             whileInView={{ y: 0, opacity: 1, backdropFilter: "blur(0px)" }}
             transition={{ delay: 0.1, duration: 0.65 }}
@@ -25,7 +25,7 @@ export default function Features() {
             </p>
           </MotionDiv>
           <MotionDiv
-            className="bg-[#948979] rounded-3xl p-10 lg:p-8 md:p-8 bg-opacity-65"
+            className="bg-[#948979] m-3 rounded-3xl p-10 lg:p-8 md:p-8 bg-opacity-65"
             initial={{ y: "100%", opacity: 0.3, backdropFilter: "blur(10px)" }}
             whileInView={{ y: 0, opacity: 1, backdropFilter: "blur(0px)" }}
             transition={{ delay: 0.2, duration: 0.65 }}
@@ -46,10 +46,10 @@ export default function Features() {
             </p>
           </MotionDiv>
           <MotionDiv
-            className="bg-slate-600  rounded-3xl p-10 lg:p-8 md:p-8  bg-opacity-65"
+            className="bg-slate-600 m-3 rounded-3xl p-10 lg:p-8 md:p-8  bg-opacity-65"
             initial={{ y: "100%", opacity: 0.3, backdropFilter: "blur(10px)" }}
             whileInView={{ y: 0, opacity: 1, backdropFilter: "blur(0px)" }}
-            transition={{ delay: 0.25, duration: 0.65 }}
+            transition={{ delay: 0.1, duration: 0.65 }}
           >
             <h1 className="lg:text-2xl md:text-2xl text-xl text-white">
               Signing Up Offers Bible Plan Follow-Ups
