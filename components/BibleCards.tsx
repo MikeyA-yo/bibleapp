abfd30c03da21e30a30e6db785cedd7d20b3598c
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-05-14 09:41:16 +0100

    anyone knows how to push stateful data from child component to parent?

--- components/BibleCards.tsx
+++ components/BibleCards.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState } from "react";
+
 export function VersesNumber({ number }: { number: number | string }) {
   return (
     <>
@@ -10,7 +14,7 @@ export function VersesNumber({ number }: { number: number | string }) {
 export function BookCards({ book }: { book: string }) {
   return (
     <>
-      <div className="flex lg:h-24 md:h-24 h-16 w-auto lg:w-44 md:w-44 lg:m-5 m-2 items-center transition duration-300 ease-in-out rounded-2xl bg-gradient-to-b from-neutral-500 via-slate-100 to-gray-300 hover:scale-110 hover:bg-gradient-to-t hover:from-gray-300 hover:via-slate-100 hover:to-neutral-500 justify-center">
+      <div className="flex lg:h-24 md:h-24 h-16 w-auto lg:w-44 md:w-44 lg:m-5 m-2 items-center transition duration-300 ease-in-out rounded-2xl bg-gradient-to-b from-neutral-500 via-slate-100 to-gray-300 hover:scale-110 hover:bg-gradient-to-t hover:from-red-300 hover:via-slate-100 hover:to-indigo-500 justify-center">
         <p className="text-base lg:text-2xl p-2 md:text-xl  text-gray-700">
           {book}
         </p>
@@ -18,8 +22,14 @@ export function BookCards({ book }: { book: string }) {
     </>
   );
 }
-
+let value: string;
+let versionValue:string;
 export function SearchBox() {
+  const [text, setText] = useState("");
+  const [version, setVersion] = useState("kjv");
+  value = text;
+  versionValue = version;
+   // console.log(version, value, versionValue, text);
   return (
     <>
       <div className="flex flex-col gap-1 w-72">
@@ -28,6 +38,10 @@ export function SearchBox() {
             className="input"
             type="text"
             placeholder="Enter key phrase in bible"
+            onChange={(e) => {
+              setText(e.target.value);
+              
+            }}
           />
           <span className="focus-border" />
         </div>
@@ -51,7 +65,12 @@ export function SearchBox() {
               className="svg-stroke-primary"
             />
           </svg>
-          <select className="appearance-none hover:placeholder-shown:bg-emerald-500 relative text-pink-400 bg-transparent ring-0 outline-none border border-neutral-500 placeholder-violet-700 text-sm font-bold rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5">
+          <select
+            onChange={(e) => {
+              setVersion(e.target.value);
+            }}
+            className="appearance-none hover:placeholder-shown:bg-emerald-500 relative text-pink-400 bg-transparent ring-0 outline-none border border-neutral-500 placeholder-violet-700 text-sm font-bold rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5"
+          >
             <option value={"kjv"}>King James Version</option>
             <option value={"nkjv"}>New king James Version</option>
             <option value={"nvi"}>NVI (portuegues)</option>
@@ -62,4 +81,7 @@ export function SearchBox() {
       </div>
     </>
   );
+}
+export function searchValues(){
+  return [value, versionValue];
 }
