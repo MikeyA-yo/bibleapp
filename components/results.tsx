595515a1a376ab7b426e74d19fb59b19a28f2bfc
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-05-24 17:54:55 +0100

    i might be just about done with search

--- components/results.tsx
+++ components/results.tsx
@@ -4,7 +4,7 @@ import { Resolved } from "./verse";
 import mapUrl from "./urlMapper";
 import Spinner from "./spinner";
 import { Montserrat } from "next/font/google";
-import { getNKJVVersesArray } from "@/app/api/fetch";
+import { ChapterArray, getNKJVVersesArray, specialChapters } from "@/app/api/fetch";
 import { useEffect, useState } from "react";
 import VerseNav from "./verseNav";
 const mont = Montserrat({ weight: ["700"], subsets: ["latin"] });
@@ -14,13 +14,51 @@ function SelectNav({
   onChange: React.ChangeEventHandler<HTMLInputElement>;
 }) {
   return (
-    <div className="pl-7">
+    <div className="pl-20">
       <p className="text-gray-300 text-sm">Show Verses Navigation</p>
       <label className="switch">
         <input type="checkbox" onChange={onChange} />
         <span className="slider">Not</span>
       </label>
     </div>
+  );
+}
+function NextChapter({
+  onClick,
+}: {
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}) {
+  return (
+    <button className="button" onClick={onClick}>
+      <span className="text text-sm">Next </span>
+      <svg
+        className="arrow"
+        viewBox="0 0 448 512"
+        height="1em"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+      </svg>
+    </button>
+  );
+}
+function PrevChapter({
+  onClick,
+}: {
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}) {
+  return (
+    <button className="button" onClick={onClick}>
+      <span className="text text-sm">Previous</span>
+      <svg
+        className="arrow-prev hover:!rotate-180 rotate-180"
+        viewBox="0 0 448 512"
+        height="1em"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+      </svg>
+    </button>
   );
 }
 export function Loading() {
@@ -41,7 +79,12 @@ export function Loading() {
 
 export default function Result() {
   const [resArray, setResArray] = useState(0);
+  //this was supposed to represent an array, but i don't know what it represents now
   const [a, setA] = useState(false);
+  //this represents the available chapters an array, where the first element represnts
+  //if there is a next chapter it is true else, false, second element 
+  //for previous if there is a previous chapter in that book returns true , else false
+  const [b, setB] = useState<any>();
 
   let sp = useSearchParams();
   const params = useParams();
@@ -53,13 +96,15 @@ export default function Result() {
     async function fetchArray() {
       const verseArray = await getNKJVVersesArray(book, chapter);
       setResArray(verseArray);
+      const ChapterState = await specialChapters(book, chapter);
+      setB(ChapterState)
     }
     fetchArray();
   }, [book, chapter]);
-
+  console.log(b)
   return (
     <>
-      <h1 className={`${mont.className} pl-20 text-2xl text-white`}>
+      <h1 className={`${mont.className} pl-20 pb-8 text-2xl text-white`}>
         {pathname.split("/")[2].replace("%20", " ")} {chapter}
       </h1>
       <SelectNav
@@ -74,6 +119,14 @@ export default function Result() {
       <div className="mx-10 rounded-xl  pb-10 my-8 bg-slate-200 bg-opacity-40 ">
         {a && resArray && <VerseNav verseNo={resArray} />}
         <Resolved version={verse} book={book} chapter={chapter} />
+        <div className="flex pt-8 justify-around items-center">
+         {b && b[1] && <PrevChapter onClick={()=>{
+          alert(b[1])
+         }} /> }
+          {b && b[0] && <NextChapter onClick={()=>{
+          alert(b[0])
+         }} /> }
+        </div>
       </div>
     </>
   );
--- components/verseNav.tsx
+++ components/verseNav.tsx
@@ -10,7 +10,7 @@ export default function VerseNav({ verseNo }: { verseNo: number }) {
     }
   return (
     <>
-      <nav className="overflow-x-auto flex items-center justify-center sticky flex-row gap-3 pb-5 whitespace-nowrap">
+      <nav className="overflow-x-auto flex lg:items-center lg:justify-center md:justify-center md:items-center sticky flex-row gap-3 pb-5 whitespace-nowrap">
          {verseArray.map((no)=>{
             return (
                <Link to={no} delay={300} duration={500} smooth spy className="cursor-pointer"> <p className="bg-red-500 bg-opacity-50 rounded-b h-6 w-6 text-sm" >{no}</p></Link>
