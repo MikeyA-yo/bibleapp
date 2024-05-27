5aa29022ac6111eefdbeadfe3962b3be77ec69be
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-05-27 21:34:52 +0100

    coming up

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,1 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
 import { createUser } from "./signup";
+export async function POST(req:NextRequest, res:NextResponse){
+    const data = await req.json();
+    createUser(data)
+    return Response.json(data)
+}
--- app/api/signup/signup.ts
+++ app/api/signup/signup.ts
@@ -3,6 +3,13 @@ import { clPromise } from "../mongodb";
 export async function createUser({email, username, password}:{email:string, username:string, password:string}){
     const client = await clPromise;
    await  client.connect()
-//    const db = await client.db('hey')
-    console.log('connected')
+   console.log('connected')
+   const db =  client.db('BibleApp');
+   const col = db.collection('Users');
+   const res = await col.insertOne({email, username, password})
+   console.log(res.insertedId, res)
+
+  // const findTest = await col.findOne({email:'b'})
+
+   
 }
