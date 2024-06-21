4018cf7038ca88d5ddd1182821fee0bc3041c06f
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-06-21 22:41:57 +0100

    testing little middleware change

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,8 @@ import type { NextRequest } from 'next/server'
 import { cookies } from "next/headers";
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  if(cookies().get("__Secure-next-auth.session-token")){
+  const cookie = cookies().get("__Secure-next-auth.session-token")
+  if(cookie){
     return NextResponse.next();
   }else{
     return NextResponse.redirect(new URL('/api/auth/signin', request.url))
